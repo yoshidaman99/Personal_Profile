ad4a7632e48f294ccfe94f29bbe37180c8c6bc5f
GitBuilder <gitbuilder@local>
2026-04-01 21:46:42 +0800

    [modified] Avatar.tsx at 2026-04-01 21:46:41

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -59,7 +59,12 @@ export default function Avatar({ state }: AvatarProps) {
     window.addEventListener("mousemove", handleMouseMove);
 
     const handleKey = (e: KeyboardEvent) => {
-      if (e.key === "a" && e.ctrlKey) setDebug((v) => !v);
+      if (e.key === "a" && e.ctrlKey) {
+        setDebug((v) => {
+          debugRef.current = !v;
+          return !v;
+        });
+      }
     };
     window.addEventListener("keydown", handleKey);
 
